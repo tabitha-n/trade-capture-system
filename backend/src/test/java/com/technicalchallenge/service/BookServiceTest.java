db48fe981eae70d5f9577a90eaa28ae4acc43168
tabitha-n <ncooro.t@gmail.com>
2025-10-24 09:54:50 +0100

    fixed findById BookService

--- backend/src/test/java/com/technicalchallenge/service/BookServiceTest.java
+++ backend/src/test/java/com/technicalchallenge/service/BookServiceTest.java
@@ -20,6 +20,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.technicalchallenge.dto.BookDTO;
+import com.technicalchallenge.mapper.BookMapper;
 import com.technicalchallenge.model.Book;
 import com.technicalchallenge.repository.BookRepository;
 
@@ -27,6 +28,8 @@ import com.technicalchallenge.repository.BookRepository;
 public class BookServiceTest {
     @Mock
     private BookRepository bookRepository;
+    @Mock
+    private BookMapper bookMapper;
     @InjectMocks
     private BookService bookService;
 
@@ -34,7 +37,12 @@ public class BookServiceTest {
     void testFindBookById() {
         Book book = new Book();
         book.setId(1L);
+
+        BookDTO dto = new BookDTO();
+        dto.setId(1L);
+        
         when(bookRepository.findById(1L)).thenReturn(Optional.of(book));
+        when(bookMapper.toDto(book)).thenReturn(dto);
         Optional<BookDTO> found = bookService.getBookById(1L);
         assertTrue(found.isPresent());
         assertEquals(1L, found.get().getId());
